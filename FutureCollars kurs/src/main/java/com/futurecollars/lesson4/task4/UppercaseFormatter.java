429c12a0c5256408faeac06ae3ce9f92f268e2b4
Patryk Mech <mechpatr@gmail.com>
2023-10-10 21:30:24 +0200

    lesson4-task4

---
+++ FutureCollars kurs/src/main/java/com/futurecollars/lesson4/task4/UppercaseFormatter.java
@@ -0,0 +1,8 @@
+package com.futurecollars.lesson4.task4;
+
+public class UppercaseFormatter implements TextFormatter{
+    @Override
+    public String formatText(String text){
+        return text.toUpperCase();
+    }
+}
