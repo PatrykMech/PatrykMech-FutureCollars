a47aa5d92b7a8161b92ecc58f3d1d5f387f83922
Patryk Mech <mechpatr@gmail.com>
2023-10-10 20:52:46 +0200

    lesson3-task4/correction

--- FutureCollars kurs/src/main/java/com/futurecollars/lesson3/task4/MinuteConverter.java
+++ FutureCollars kurs/src/main/java/com/futurecollars/lesson3/task4/MinuteConverter.java
@@ -1,12 +1,12 @@
 package com.futurecollars.lesson3.task4;
 
 public class MinuteConverter {
-    static int converter_multiply = 60;
+    private static final int SECONDS_IN_A_MINUTE = 60;
     public static int convertMinutesToSeconds(int minutes) {
         if (minutes < 0) {
             throw new IllegalArgumentException("Minutes can't be negative");
         } else {
-            return minutes * converter_multiply;
+            return minutes * SECONDS_IN_A_MINUTE;
         }
     }
 }
