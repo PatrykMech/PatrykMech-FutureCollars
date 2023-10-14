7802dfb4fa62ca9760636cbb3e93ac1ad523ef5f
Patryk Mech <mechpatr@gmail.com>
2023-10-14 14:20:01 +0200

    lesson4-task2/correction4

--- FutureCollars kurs/src/main/java/com/futurecollars/lesson4/task2/Square.java
+++ FutureCollars kurs/src/main/java/com/futurecollars/lesson4/task2/Square.java
@@ -5,14 +5,4 @@ class Square extends Rectangle {
     public Square(int side) {
         super(side, side);
     }
-
-    @Override
-    public double getArea() {
-        return length * length;
-    }
-
-    @Override
-    public double getPerimeter() {
-        return 4 * length;
-    }
 }
