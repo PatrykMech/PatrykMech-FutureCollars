ca9eaed18ee2d23e5f0e567ce3523529cc65889b
Patryk Mech <mechpatr@gmail.com>
2023-10-27 14:55:08 +0200

    lesson9-task2/correction1

--- FutureCollars kurs/src/main/java/com/futurecollars/lesson9/task2/OwnQueueImplementation.java
+++ FutureCollars kurs/src/main/java/com/futurecollars/lesson9/task2/OwnQueueImplementation.java
@@ -1,35 +1,77 @@
 package com.futurecollars.lesson9.task2;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 public class OwnQueueImplementation implements OwnQueue {
-    private final List<Integer> queue = new ArrayList<>();
+    private Node front;
+    private Node back;
+    private int size;
 
     @Override
     public boolean add(Integer e) {
         if (e == null) {
             throw new NullPointerException("Element cannot be null");
         }
-        queue.add(e);
+
+        Node node = new Node(e);
+
+        if (size == 0) {
+            front = node;
+        } else {
+            back.setNext(node);
+        }
+
+        back = node;
+        size++;
+
         return true;
     }
 
     @Override
     public Integer remove() {
-        if (queue.isEmpty()) {
+        if (isEmpty()) {
             throw new NoSuchElementException("Queue is empty");
         }
-        return queue.remove(0);
+
+        Integer value = front.getValue();
+        front = front.getNext();
+        size--;
+
+        return value;
     }
 
     @Override
     public Integer element() {
-        if (queue.isEmpty()) {
+        if (isEmpty()) {
             throw new NoSuchElementException("Queue is empty");
         }
-        return queue.get(0);
+
+        return front.getValue();
+    }
+
+    private boolean isEmpty() {
+        return size == 0;
+    }
+
+    private static class Node {
+        private final Integer value;
+        private Node next;
+
+        public Node(Integer value) {
+            this.value = value;
+            this.next = null;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
     }
 }
-
